d933fafac15bdc1b2a33f4b635adf91a7fbe79f2
Augusto Ciuffoletti <augusto@di.unipi.it>
2017-03-30 14:13:40 +0200

    Aggiunto il passo di incremento

--- rip.py
+++ rip.py
@@ -6,12 +6,16 @@ from collections import defaultdict
 def merge(routing,r_from,advertisement):
     # Ricopio la tabella locale nell'output
     t_out=defaultdict(list)
-    for dest in routing.keys(): t_out[dest]=routing[dest]
+    for dest in advertisement.keys(): 
+		advertisement[dest]=advertisement[dest]+1
+    for dest in routing.keys(): 
+		t_out[dest]=routing[dest]
     # Scansione della tabella in input
     for dest in advertisement.keys():
         # Se la destinazione e' nota
         if dest in t_out.keys():
-            # e il prossimo router e' quello da cui ricevo la tabella, aggiorno il costo
+            # e il prossimo router e' quello da cui ricevo la tabella, 
+            # aggiorno il costo
             if t_out[dest][0] == r_from: t_out[dest]=[r_from,advertisement[dest]]
             # altrimenti, se il costo e' inferiore aggiorno il costo e il router
             elif t_out[dest][1] > advertisement[dest]: 
@@ -26,9 +30,8 @@ tabella_routing={
  'n2':['C',1],
  'n3':['A',3],
  'n4':['B',1],
- 'n5':['B',2],
- 'n6':['C',5]}
+ 'n5':['B',2]}
 
-out = merge(tabella_routing,'C',{'n2':2,'n3':1,'n6':5})
+out = merge(tabella_routing,'C',{'n2':1,'n3':0,'n6':4})
  
 print(out)
